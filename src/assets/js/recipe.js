566fdcaa0285ffc3a415d19d2a2c370e180066fe
Fauzi Kliman <fauzimerfoo@gmail.com>
2017-05-11 19:07:08 -0700

    Improved structure of recipe data in database

--- src/assets/js/recipe.js
+++ src/assets/js/recipe.js
@@ -3,63 +3,26 @@ import { database } from '@/assets/js/firebase/index';
 export default {
   create(title, content, userId) {
     const ref = database.ref();
-    const userQuery = ref.child(`users/${userId}/recipesMade`).push();
-    const recipeKeyQuery = ref.child('recipeKeys').push();
-    const recipeQuery = ref.child('recipes').push();
-    const titleQuery = ref.child('recipeTitles').push();
-    const contentQuery = ref.child('recipeContents').push();
+    const recipeKey = ref.child('recipes').push({ userId }).key;
 
-    userQuery.set({
-      recipeKey: recipeQuery.key,
-    });
-
-    recipeKeyQuery.set({
-      recipeKey: recipeQuery.key,
-    });
-
-    recipeQuery.set({
-      titleKey: titleQuery.key,
-      contentKey: contentQuery.key,
-      userId,
-    });
-
-    titleQuery.set({
-      title,
-      recipeKey: recipeQuery.key,
-    });
-
-    contentQuery.set({
-      content,
-      recipeKey: recipeQuery.key,
-    });
+    ref.child(`recipeTitles/${recipeKey}`).set({ title });
+    ref.child(`recipeContents/${recipeKey}`).set({ content });
+    ref.child(`users/${userId}/recipesMade/${recipeKey}`).set({ recipeKey });
   },
   get(recipeKey) {
     return new Promise((resolve, reject) => {
       const ref = database.ref();
+      const titlePromise = ref.child(`recipeTitles/${recipeKey}`).once('value');
+      const contentPromise = ref.child(`recipeContents/${recipeKey}`).once('value');
 
-      ref.child(`recipes/${recipeKey}`).once('value').then((recipeData) => {
-        const recipe = recipeData.val();
-        const titlePromise = ref.child(`recipeTitles/${recipe.titleKey}`).once('value');
-        const contentPromise = ref.child(`recipeContents/${recipe.contentKey}`).once('value');
-
-        Promise.all([titlePromise, contentPromise]).then((data) => {
-          resolve({
-            title: data[0].val().title,
-            content: data[1].val().content,
-          });
-        }, (error) => {
-          console.log(error);
-          reject({
-            title: 'Error',
-            content: 'Error',
-          });
+      Promise.all([titlePromise, contentPromise]).then((recipeData) => {
+        resolve({
+          title: recipeData[0].val().title,
+          content: recipeData[1].val().content,
         });
       }, (error) => {
         console.log(error);
-        reject({
-          title: 'Error',
-          content: 'Error',
-        });
+        reject(error);
       });
     });
   },
@@ -67,17 +30,13 @@ export default {
     return new Promise((resolve, reject) => {
       const ref = database.ref();
 
-      ref.child(`recipes/${recipeKey}/titleKey`).once('value').then((titleKeyData) => {
-        ref.child(`recipeTitles/${titleKeyData.val()}`).once('value').then((titleData) => {
-          resolve({
-            title: titleData.val().title,
-          });
+      ref.child(`recipeTitles/${recipeKey}`).once('value').then((titleData) => {
+        resolve({
+          title: titleData.val().title,
         });
       }, (error) => {
         console.log(error);
-        reject({
-          title: 'Error',
-        });
+        reject(error);
       });
     });
   },
@@ -85,18 +44,18 @@ export default {
     return new Promise((resolve, reject) => {
       const ref = database.ref();
 
-      ref.child('recipeKeys').limitToLast(5).once('value').then((recipeDatas) => {
+      ref.child('recipes').limitToLast(5).once('value').then((recipeDatas) => {
         const recipes = [];
         const recipeDatasVal = recipeDatas.val();
 
         Object.keys(recipeDatasVal).forEach((key) => {
-          recipes.push(recipeDatasVal[key].recipeKey);
+          recipes.push(key);
         });
 
         resolve(recipes);
       }, (error) => {
         console.log(error);
-        reject([]);
+        reject(error);
       });
     });
   },
